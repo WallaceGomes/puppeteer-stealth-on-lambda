0e63768b29a1b9c7258c3725a0324a0e4a02d990
Jordan Hansen <jbhansen84@gmail.com>
2021-11-12 15:17:43 -0700

    add captcha solving and readme

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Cool Puppeteer things on Lambda (stealth and solving captchas)
+
+This package has some examples of how to run Puppeteer on Lambda in stealth mode (so stealthy) and also solving both reCaptcha and simple image captchas.
+
+## Getting Started
+
+Clone the repository and run `npm i`.
+
+I have a neat script in the package.json that allows you to easily push to Lambda. You'll need to create and rename the s3 bucket and rename the lambda function name to match your own.
+
+Then...just comment in the function you want to use and run it!
+
+[Video: Setting up Puppeteer Stealth on Lambda](https://www.youtube.com/watch?v=pkqIEv9i1KY)
+[Video: How to Solve reCaptchas with Puppeteer](https://www.youtube.com/watch?v=D52NjoZWn14)
+[Video: How to Solve Simple Captchas with Puppeteer](https://www.youtube.com/watch?v=r-ANFgW5Y3o)
+
+### Prerequisites
+
+Tested on Node v14.17.5 and NPM v6.14.14.
+
+### Installing
+
+After installing [NodeJS](https://nodejs.org/en/) you should be able to just run the following in the terminal.
+
+```
+npm i
+```
+
+## Built With
+
+* [NodeJS](https://nodejs.org/en/) - NodeJS
+* [2Captcha](https://2captcha.com?from=7390140) - NodeJS
+* [puppeteer](https://github.com/puppeteer/puppeteer) - Headless browser
+* [puppeteer-extra](https://github.com/berstend/puppeteer-extra) - Amazing package for extra puppeteer plugins
+* [chrome-aws-lambda](https://github.com/alixaxel/chrome-aws-lambda) - Allows you to run Puppeteer on lambda
+* [@infosimples/node_two_captcha](https://github.com/infosimples/node_two_captcha) - Easy way to interface with 2Captcha
+
+## Authors
+
+* **Jordan Hansen** - *Initial work* - [Jordan Hansen](https://github.com/aarmora)
+
+
+## License
+
+This project is licensed under the ISC License
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,114 @@
 import chromium from 'chrome-aws-lambda';
 import { addExtra } from 'puppeteer-extra';
 import pluginStealth from 'puppeteer-extra-plugin-stealth';
+import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
+import Client from '@infosimples/node_two_captcha';
 
 export async function handler() {
+    // await solveRecaptcha();
+
+    await solveSimpleCatcha();
+}
+
+async function solveSimpleCatcha() {
+    const captchaDecoderClient = new Client(process.env.captchaToken, {
+        timeout: 60000,
+        polling: 5000,
+        throwErrors: false
+    });
+    const puppeteerExtra = addExtra(chromium.puppeteer as any);
+    puppeteerExtra.use(pluginStealth());
+    const browser = await puppeteerExtra
+        .launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless
+        });
+    const url = 'https://quickstart.sos.nh.gov/online/BusinessInquire';
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const image = await page.screenshot({
+        // This should be the region where the simple captcha is located
+        clip: { x: 625, y: 400, width: 110, height: 35 },
+        encoding: 'base64'
+    });
+
+    console.log('base 64 captcha image', image);
+
+    const decodedResponse = await captchaDecoderClient.decode({
+        base64: image
+    });
+
+    console.log('decodedResponse', decodedResponse._text);
+    await page.type('#txtCaptcha', decodedResponse._text.toLocaleUpperCase());
+    await page.click('#rbContains');
+    await page.type('#txtBusinessName', 'pizza');
+    await page.click('#btnSearch');
+    await page.waitForSelector('#tdBusinessSearch', { timeout: 10000 });
+
+    const businessName = await page.$eval('#tdBusinessSearch tbody tr:nth-of-type(1) td:nth-of-type(1)', element => element.textContent);
+    console.log('businessName', businessName);
+
+    await browser.close();
+}
+
+async function solveRecaptcha() {
+    const puppeteerExtra = addExtra(chromium.puppeteer as any);
+    puppeteerExtra.use(pluginStealth());
+    puppeteerExtra.use(
+        RecaptchaPlugin({
+            provider: { id: '2captcha', token: process.env.captchaToken },
+            visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+        })
+    );
+
+    const browser = await puppeteerExtra
+        .launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless
+        });
+
+    const url = 'https://tnbear.tn.gov/Ecommerce/FilingSearch.aspx';
+
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    // Captcha is usually there but not always.
+    let captcha = false;
+    try {
+        await page.waitForSelector('.g-recaptcha', { timeout: 5000 });
+        captcha = true;
+    } catch (e) {
+        console.log('No captcha here');
+        captcha = false;
+    }
+    if (captcha) {
+        try {
+            console.log('About to solve captcha');
+            await page.solveRecaptchas();
+            console.log('Captcha solved, moving on');
+        } catch (e) {
+            console.log('Error happened when trying to solve captcha.');
+            throw e;
+        }
+    }
+
+    await page.type('#ctl00_MainContent_txtSearchValue', 'pizza');
+
+    await page.click('#ctl00_MainContent_SearchButton');
+    await page.waitForSelector('#ctl00_MainContent_SearchResultList');
+    const businessName = await page.$eval('#ctl00_MainContent_SearchResultList tbody tr:nth-of-type(2) td:nth-of-type(3)', element => element.textContent);
+
+    console.log('business name', businessName);
+
+    await browser.close();
+}
+
+async function testStealth() {
     const puppeteerExtra = addExtra(chromium.puppeteer as any);
     puppeteerExtra.use(pluginStealth());
 
@@ -32,7 +138,7 @@ export async function handler() {
         const result = await row.$eval('td:nth-of-type(2)', element => element.textContent);
 
         console.log('Test:', test, 'Result:', result);
-        
+
     }
 
     await browser.close();
