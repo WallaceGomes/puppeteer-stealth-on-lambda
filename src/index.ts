9b1fd3212bf145d307847581917815348c0c18db
WallaceGomes <wallacecardosogomes@gmail.com>
2021-12-10 23:19:22 -0300

    chore: add rpas azul e latam

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
 import chromium from 'chrome-aws-lambda';
-import { addExtra } from 'puppeteer-extra';
+import recaptcha from 'puppeteer-extra-plugin-recaptcha';
 import pluginStealth from 'puppeteer-extra-plugin-stealth';
-import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
-import Client from '@infosimples/node_two_captcha';
+import { addExtra } from 'puppeteer-extra';
+const USER_AGENT =
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36';
 
-export async function handler() {
-    // await solveRecaptcha();
 
-    await solveSimpleCatcha();
+exports.handler = async function (event) {
+    const { queryStringParameters } = event;
+    const { user, pass, url, program } = queryStringParameters;
+
+    let response;
+
+    try {
+        switch (program) {
+            case 'azul':
+                response = await scrapAzul(user, pass, url);
+                break;
+            case 'latam':
+                response = await scrapLatam(user, pass, url);
+        
+            default:
+                response = await scrap(user, pass, url);
+                break;
+        }
+
+    } catch (error) {
+        response = {
+			error:
+				'Ocorreu um erro durante a operação. Cheque seus dados e tente novamente.',
+		};
+    }
+
+    return response;
 }
 
-async function solveSimpleCatcha() {
-    const captchaDecoderClient = new Client(process.env.captchaToken, {
-        timeout: 60000,
-        polling: 5000,
-        throwErrors: false
-    });
-    const puppeteerExtra = addExtra(chromium.puppeteer as any);
+async function scrap(user, pass, url) {
+	const puppeteerExtra = addExtra(chromium.puppeteer as any);
     puppeteerExtra.use(pluginStealth());
+
     const browser = await puppeteerExtra
         .launch({
             args: chromium.args,
@@ -25,43 +46,100 @@ async function solveSimpleCatcha() {
             executablePath: await chromium.executablePath,
             headless: chromium.headless
         });
-    const url = 'https://quickstart.sos.nh.gov/online/BusinessInquire';
-    const page = await browser.newPage();
+
+	const page = await browser.newPage();
+
+    page.setDefaultNavigationTimeout(30000);
+
+    console.log(`Loading page...:${url}`);
     await page.goto(url);
 
-    const image = await page.screenshot({
-        // This should be the region where the simple captcha is located
-        clip: { x: 625, y: 400, width: 110, height: 35 },
-        encoding: 'base64'
-    });
-
-    console.log('base 64 captcha image', image);
-
-    const decodedResponse = await captchaDecoderClient.decode({
-        base64: image
-    });
-
-    console.log('decodedResponse', decodedResponse._text);
-    await page.type('#txtCaptcha', decodedResponse._text.toLocaleUpperCase());
-    await page.click('#rbContains');
-    await page.type('#txtBusinessName', 'pizza');
-    await page.click('#btnSearch');
-    await page.waitForSelector('#tdBusinessSearch', { timeout: 10000 });
-
-    const businessName = await page.$eval('#tdBusinessSearch tbody tr:nth-of-type(1) td:nth-of-type(1)', element => element.textContent);
-    console.log('businessName', businessName);
-
+    console.log('page loaded!')
     await browser.close();
+
+    return { miles: 'ok', expiryDate: 'ok' };
 }
 
-async function solveRecaptcha() {
+async function scrapAzul(user, pass, url) {
+
+    const puppeteerExtra = addExtra(chromium.puppeteer as any);
+
+	const browser = await puppeteerExtra.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+    });
+
+    //página de extrato https://tudoazul.voeazul.com.br/group/azul/activity-history
+    //página de login https://apps.voeazul.com.br/TudoAzulRenew/
+
+	const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(30000);
+
+    console.log(`Loading page...:${url}`);
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    await page.waitForSelector('#agentName');
+    console.log('type login');
+    await page.type('#agentName', user, {
+        delay: 50,
+    });
+    await page.waitForSelector('#password');
+    console.log('type password');
+    await page.type('#password', pass, {
+        delay: 50,
+    });
+    await page.click('#btnEfetuarLogin');
+    console.log('click login');
+
+    //if the form has some error the rpa will not find the button
+    // await page.waitForSelector(
+    //     'a.btn.btn-block.btn-outline.btn-outline-light',
+    //     { timeout: 30000 },
+    // );
+    // await page.click('a.btn.btn-block.btn-outline.btn-outline-light');
+
+    await page.goto('https://tudoazul.voeazul.com.br/group/azul/activity-history', { waitUntil: 'networkidle2' });
+
+    await page.waitForSelector('#balance');
+    const balance = await page.evaluate(() => {
+        const auxBalance = document.getElementById('balance');
+        const miles = auxBalance.textContent;
+        return miles;
+    });
+
+    // const expDate = await page.evaluate(() => {
+    // 	const auxDate = document.querySelector('.ng-binding.ng-scope');
+    // 	const date = auxDate.innerText;
+    // 	if (date) {
+    // 		return date.replace('A vencer (', '').replace(')', '').replace(':', '');
+    // 	}
+    // });
+
+    const expDate = page.$eval('.ng-binding.ng-scope', element => element.innerHTML);
+
+    console.log(`Milhas : ${balance}`);
+    console.log(`Date : ${expDate}`);
+    console.log('finish');
+
+    await browser.close();
+
+    return { miles: balance, expiryDate: expDate };
+}
+
+async function scrapLatam(user, pass, url) {
     const puppeteerExtra = addExtra(chromium.puppeteer as any);
     puppeteerExtra.use(pluginStealth());
     puppeteerExtra.use(
-        RecaptchaPlugin({
-            provider: { id: '2captcha', token: process.env.captchaToken },
-            visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
-        })
+        recaptcha({
+            provider: {
+                id: '2captcha',
+                token: process.env.CAPTCHA_KEY,
+            },
+            visualFeedback: true,
+        }),
     );
 
     const browser = await puppeteerExtra
@@ -72,74 +150,102 @@ async function solveRecaptcha() {
             headless: chromium.headless
         });
 
-    const url = 'https://tnbear.tn.gov/Ecommerce/FilingSearch.aspx';
+	const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(30000);
+    await page.setUserAgent(USER_AGENT);
+	await page.setJavaScriptEnabled(true);
+	await page.setDefaultTimeout(0);
 
-    const page = await browser.newPage();
+	// Randomize viewport size
+	await page.setViewport({
+		width: 1200 + Math.floor(Math.random() * 100),
+		height: 720 + Math.floor(Math.random() * 100),
+		deviceScaleFactor: 1,
+		hasTouch: false,
+		isLandscape: false,
+		isMobile: false,
+	});
 
-    await page.goto(url);
-    // Captcha is usually there but not always.
-    let captcha = false;
-    try {
-        await page.waitForSelector('.g-recaptcha', { timeout: 5000 });
-        captcha = true;
-    } catch (e) {
-        console.log('No captcha here');
-        captcha = false;
-    }
-    if (captcha) {
-        try {
-            console.log('About to solve captcha');
-            await page.solveRecaptchas();
-            console.log('Captcha solved, moving on');
-        } catch (e) {
-            console.log('Error happened when trying to solve captcha.');
-            throw e;
-        }
-    }
+	// Skip images/styles/fonts loading for performance
+	await page.setRequestInterception(true);
+	page.on('request', (req) => {
+		if (
+			req.resourceType() == 'stylesheet' ||
+			req.resourceType() == 'font' ||
+			req.resourceType() == 'image'
+		) {
+			req.abort();
+		} else {
+			req.continue();
+		}
+	});
 
-    await page.type('#ctl00_MainContent_txtSearchValue', 'pizza');
+	await page.evaluateOnNewDocument(() => {
+		// Pass webdriver check
+		Object.defineProperty(navigator, 'webdriver', {
+			get: () => false,
+		});
+	});
 
-    await page.click('#ctl00_MainContent_SearchButton');
-    await page.waitForSelector('#ctl00_MainContent_SearchResultList');
-    const businessName = await page.$eval('#ctl00_MainContent_SearchResultList tbody tr:nth-of-type(2) td:nth-of-type(3)', element => element.textContent);
+	await page.evaluateOnNewDocument(() => {
+		// Overwrite the `plugins` property to use a custom getter.
+		Object.defineProperty(navigator, 'plugins', {
+			// This just needs to have `length > 0` for the current test,
+			// but we could mock the plugins too if necessary.
+			get: () => [1, 2, 3, 4, 5],
+		});
+	});
 
-    console.log('business name', businessName);
+	await page.evaluateOnNewDocument(() => {
+		// Overwrite the `languages` property to use a custom getter.
+		Object.defineProperty(navigator, 'languages', {
+			get: () => ['en-US', 'en'],
+		});
+	});
 
-    await browser.close();
-}
+    console.log(`Loading page...:${url}`);
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
-async function testStealth() {
-    const puppeteerExtra = addExtra(chromium.puppeteer as any);
-    puppeteerExtra.use(pluginStealth());
+    await page.goto('https://latampass.latam.com/pt_br/');
 
-    const browser = await puppeteerExtra
-        .launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless
-        });
+    await page.waitForSelector('#auth0-login');
+    await page.click('#auth0-login');
+    await page.waitForSelector('#form-input--alias');
+    console.log('type login');
+    await page.type('#form-input--alias', user, {
+        delay: 50,
+    });
+    await page.waitForSelector('#form-input--password');
+    console.log('type password');
+    await page.type('#form-input--password', pass, {
+        delay: 50,
+    });
+    // await page.waitForTimeout(90000);
+    await page.click('#form-button--submit');
+    console.log('click login');
 
-    const page = await browser.newPage();
+    // console.log(
+    // 	'Loading page...:https://www.pontosmultiplus.com.br/portal/pages/home.html',
+    // );
 
-    await page.goto('https://bot.sannysoft.com/');
-    const rows = await page.$$('table:nth-of-type(1) tr');
+    // // console.log('EXP page...');
+    // await page.goto(
+    // 	'https://www.pontosmultiplus.com.br/portal/pages/home.html',
+    // );
 
-    for (let i = 0; i < rows.length; i++) {
-        const row = rows[i];
-        const handleTest = await row.$('th');
+    await page.waitForSelector('#dropdownMenuButton');
+    console.log(
+        'Loading page...:https://www.pontosmultiplus.com.br/portal/pages/home.html',
+    );
 
-        if (handleTest) {
-            console.log('We are in a header row, skipping');
-            continue;
-        }
+    // console.log('EXP page...');
+    await page.goto(
+        'https://www.pontosmultiplus.com.br/portal/pages/home.html',
+    );
 
-        const test = await row.$eval('td:nth-of-type(1)', element => element.textContent);
-        const result = await row.$eval('td:nth-of-type(2)', element => element.textContent);
+    await page.waitForSelector('#info-box-points');
 
-        console.log('Test:', test, 'Result:', result);
+    await page.waitForTimeout(500);
 
-    }
-
-    await browser.close();
+    return { miles: 'ok', expiryDate: 'ok' };
 }
